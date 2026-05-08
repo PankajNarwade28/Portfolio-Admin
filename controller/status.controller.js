fc5237955ca9b99079e61b4bca900f2fb0801f6e
PankajNarwade28 <pankajnarwade258@gmail.com>
2026-05-08 17:11:12 +0530

    added availablity route for checking status

--- controller/status.controller.js
+++ controller/status.controller.js
@@ -31,4 +31,28 @@ const getDatabaseStatus = async (req, res) => {
   }
 };
 
-export { getBackendStatus, getDatabaseStatus };
+
+// ✅ Get Availability Status
+const getAvailabilityStatus = async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("personal_info")
+      .select("is_available")
+      .single();
+
+    if (error) throw error;
+
+    res.status(200).json({
+      success: true,
+      is_available: data.is_available,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch availability status",
+      error: err.message,
+    });
+  }
+}; 
+
+export { getBackendStatus, getDatabaseStatus , getAvailabilityStatus};
--- routes/status.routes.js
+++ routes/status.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';   
-import { getBackendStatus, getDatabaseStatus } from '../controller/status.controller.js';
+import { getBackendStatus, getDatabaseStatus , getAvailabilityStatus } from '../controller/status.controller.js';
 const router = express.Router();
 
 // ✅ Backend Health Check
 router.get("/backend", getBackendStatus);
 // ✅ Database (Supabase) Health Check
 router.get("/database", getDatabaseStatus);
+
+router.get("/availability", getAvailabilityStatus);
 
 export default router;
